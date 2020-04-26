c69d79009f3e4bc0bf3dcdeacbd4b47ff372a60b
jakejake2115 <43522341+jakejake2115@users.noreply.github.com>
2020-04-26 14:15:16 -0700

    V1
    Has working gui with Toast making set alarm button

---
+++ src/main/java/edu/ilstu/MainActivity.java
@@ -0,0 +1,31 @@
+package edu.ilstu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    TextView t1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1 = findViewById(R.id.button);
+        TextView t1 = findViewById(R.id.title);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Alarm Set", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
